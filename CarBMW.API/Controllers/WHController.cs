f8fae16f1a6b0eb93127bb77225a2254a78aadae
谢震 <46702380+hardshells@users.noreply.github.com>
2020-09-17 17:23:18 +0800

    WHCont

---
+++ CarBMW.API/Controllers/WHController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBMW.API.Controllers
+{
+    [EnableCors("cors")]
+    [ApiController]
+    public class WHController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
